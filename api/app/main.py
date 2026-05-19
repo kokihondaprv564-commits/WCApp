63cb2c80887a2f8eef9f7c96be23a317abba8ade
Koki Honda <kokihondaprv564@gmail.com>
2026-05-20 07:28:23 +0900

    CORS設定を追加

--- api/app/main.py
+++ api/app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi import APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import router as players_router
 
 app = FastAPI(title="WCApp API")
+
+# 開発用に簡易的な CORS 設定を追加
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
